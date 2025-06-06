f6abf5a8bc44c49f40e8c394849d1bfad5971a4a
Atharva Kalaskar <atharvakalaskar95@gmail.com>
2025-06-06 10:46:02 +0530

    Index Route Added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wandrly";
 const Listing = require("./Models/listing.js");
+const path = require("path");
+
 
 main()
     .then((res) => {
@@ -17,23 +19,32 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
     res.send("This is root directory");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My new villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Mumbai",
-        country: "India"
-    });
+// Index Route
+app.get("/listings", async (req, res) => {
+    let allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+})
 
-    await sampleListing.save();
-    console.log("Sample saved");
-    res.send("Successful");
-});
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Mumbai",
+//         country: "India"
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample saved");
+//     res.send("Successful");
+// });
 
 app.listen(8080, () => {
     console.log("App is listening on port 8080");
