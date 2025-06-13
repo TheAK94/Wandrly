23de5702d5f4b27f2ebff2a186ad17bda1360493
Atharva Kalaskar <atharvakalaskar95@gmail.com>
2025-06-13 19:40:57 +0530

    added tax switch feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,18 @@
 (() => {
-  'use strict'
+	'use strict'
 
-  const forms = document.querySelectorAll('.needs-validation')
+  	const forms = document.querySelectorAll('.needs-validation')
 
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+  	Array.from(forms).forEach(form => {
+    	form.addEventListener('submit', event => {
+			if (!form.checkValidity()) {
+			event.preventDefault()
+			event.stopPropagation()
+		}
 
-      form.classList.add('was-validated')
-    }, false)
-  })
+		form.classList.add('was-validated')
+		}, false)
+	})
 })()
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -24,3 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
         toast.show();
     }
 });
+
+
+let taxSwitch = document.getElementById("switchCheckDefault");
+taxSwitch.addEventListener("click", () => {
+    let taxInfo = document.getElementsByClassName("tax-info");
+	for (info of taxInfo) {
+		if (info.style.display != "none") {
+			info.style.display = "none";
+		} else {
+			info.style.display = "inline";
+		}
+		
+	}
+})
